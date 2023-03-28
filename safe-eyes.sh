0e606414febd8a5ab4e6d992ebc32c8f1dbf0100
Rafał Garbowski <rg9@rg9.pl>
2023-03-29 00:46:57 +0200

    Cleanup: format code

--- safe-eyes.sh
+++ safe-eyes.sh
@@ -7,20 +7,19 @@
 #########################################################################################
 
 # ----------- CONFIG ---------------
-WORK_TIME_IN_SECONDS=$((20*60))
-BREAK_TIME_IN_SECONDS=$((1*60))
+WORK_TIME_IN_SECONDS=$((20 * 60))
+BREAK_TIME_IN_SECONDS=$((1 * 60))
 # ----------------------------------
 
-_break_countdown(){
-    for i in `seq 1 $BREAK_TIME_IN_SECONDS`;
-    do
-        LAST_LOCK=$(_seconds_since_last_event 'screenlocker')
-        if (( BREAK_TIME_IN_SECONDS > LAST_LOCK )); then
-          pkill -f "safe-eyes-break"
-        fi
-        echo $((100*i/BREAK_TIME_IN_SECONDS))
-        sleep 1
-    done
+_break_countdown() {
+  for i in $(seq 1 $BREAK_TIME_IN_SECONDS); do
+    LAST_LOCK=$(_seconds_since_last_event 'screenlocker')
+    if ((BREAK_TIME_IN_SECONDS > LAST_LOCK)); then
+      pkill -f "safe-eyes-break"
+    fi
+    echo $((100 * i / BREAK_TIME_IN_SECONDS))
+    sleep 1
+  done
 }
 
 _break_dialog() {
@@ -32,9 +31,9 @@ _break_dialog() {
   # TODO "GDK_BACKEND=x11" is workaround on Wayland to prevent minimizing break dialog (i.e. skip break) trough shortcuts like "showing desktop (Win+D)"
   # original "safeeyes" locks keyboard: https://github.com/slgobinath/SafeEyes/blob/master/safeeyes/ui/break_screen.py#L239
   # possible solution: fork process to focus on break dialog using "wmctrl" every 1s
-  _break_countdown | GDK_BACKEND=x11 yad --progress --title="safe-eyes-break" --no-escape --sticky --on-top  --undecorated --skip-taskbar --fullscreen  --timeout="${BREAK_TIME_IN_SECONDS}" \
-  --text="\n${BREAK_TIME_IN_SECONDS} seconds break"  --hide-text --text-align=center --css='*{background-color: #31363b; color: #fcfcfc; font: 25px Sans;}' \
-  --no-buttons
+  _break_countdown | GDK_BACKEND=x11 yad --progress --title="safe-eyes-break" --no-escape --sticky --on-top --undecorated --skip-taskbar --fullscreen --timeout="${BREAK_TIME_IN_SECONDS}" \
+    --text="\n${BREAK_TIME_IN_SECONDS} seconds break" --hide-text --text-align=center --css='*{background-color: #31363b; color: #fcfcfc; font: 25px Sans;}' \
+    --no-buttons
 
   _beep_end_of_break
 }
@@ -59,22 +58,21 @@ _seconds_since_last_event() {
   # https://superuser.com/questions/357275/how-to-find-the-uptime-since-last-wake-from-standby
   LAST_LOGIN_TIMESTAMP=$(journalctl -n1 --grep "$event_keyword" -o short-unix | cut -d. -f1)
   TIMESTAMP=$(date +%s)
-  DIFF=$((TIMESTAMP-LAST_LOGIN_TIMESTAMP))
+  DIFF=$((TIMESTAMP - LAST_LOGIN_TIMESTAMP))
   echo $DIFF
 }
 
 _postpone_break_if_screen_was_locked() {
   LAST_LOGIN=$(_seconds_since_last_event 'USER_AUTH')
-  POSTPONE=$((WORK_TIME_IN_SECONDS-LAST_LOGIN))
-  if (( POSTPONE > 0 )); then
+  POSTPONE=$((WORK_TIME_IN_SECONDS - LAST_LOGIN))
+  if ((POSTPONE > 0)); then
     echo "Detected locked screen - postponing break by $POSTPONE seconds"
     sleep ${POSTPONE}
   fi
 }
 
 _main_program() {
-  while true
-  do
+  while true; do
     sleep ${WORK_TIME_IN_SECONDS}
 
     _postpone_break_if_screen_was_locked
@@ -84,22 +82,25 @@ _main_program() {
 }
 
 case "$1" in
-  start)
-    if [[ `pgrep -cf "$(basename $0) start"` -gt 1 ]]; then
-      echo "Already started - skipping"
-      exit 0
-    fi
-    _main_program 2>& 1 & # run in background
-    exit 0 ;;
-  stop)
-    pkill -e -9 -f "$(basename $0) start"
-    exit 0 ;;
-  help | --help | -help | "") # prints this help
-    echo "Usage: $0 [start|stop]"
-    echo
-    echo "Current configuration:"
-    echo "\$WORK_TIME_IN_SECONDS: $WORK_TIME_IN_SECONDS"
-    echo "\$BREAK_TIME_IN_SECONDS: $BREAK_TIME_IN_SECONDS"
-    echo "(edit source code in order to change config)"
-    exit 0 ;;
+start)
+  if [[ $(pgrep -cf "$(basename $0) start") -gt 1 ]]; then
+    echo "Already started - skipping"
+    exit 0
+  fi
+  _main_program 2>&1 & # run in background
+  exit 0
+  ;;
+stop)
+  pkill -e -9 -f "$(basename $0) start"
+  exit 0
+  ;;
+help | --help | -help | "") # prints this help
+  echo "Usage: $0 [start|stop]"
+  echo
+  echo "Current configuration:"
+  echo "\$WORK_TIME_IN_SECONDS: $WORK_TIME_IN_SECONDS"
+  echo "\$BREAK_TIME_IN_SECONDS: $BREAK_TIME_IN_SECONDS"
+  echo "(edit source code in order to change config)"
+  exit 0
+  ;;
 esac
